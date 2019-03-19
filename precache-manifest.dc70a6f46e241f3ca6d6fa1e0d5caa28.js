32671cd7a1add442e23b520db7bcfe4302604112
Kalani-PC\Kalani <ahloykm@gmail.com>
2019-03-18 23:56:39 -0500

    Updates

---
+++ precache-manifest.dc70a6f46e241f3ca6d6fa1e0d5caa28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec4128b0bd53815d7482",
+    "url": "/cm/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "ec4128b0bd53815d7482",
+    "url": "/cm/static/js/main.e4c736cc.chunk.js"
+  },
+  {
+    "revision": "3439bce1679d4e0f0448",
+    "url": "/cm/static/js/runtime~main.83155044.js"
+  },
+  {
+    "revision": "42bbb636b2e611902565",
+    "url": "/cm/static/css/2.9b36699e.chunk.css"
+  },
+  {
+    "revision": "42bbb636b2e611902565",
+    "url": "/cm/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "56cd9e05d9e595fe279c67ba8b5c891d",
+    "url": "/cm/index.html"
+  }
+];
